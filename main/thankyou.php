d4ae9411dccac8e277ac3f5512937ba360a5e214
AileenF <ggimenez@academic.rrc.ca>
2023-04-10 17:54:37 -0500

    thank you page for applying the job

---
+++ main/thankyou.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="career.css">
+    <title>Thank you for your application</title>
+    <div class="title">
+    <img id="logo" src="images/logo.png" alt="top">
+</div>
+<div class="topnav">
+  <nav>
+    <a href="index.php">Back to Home</a> 
+  </nav>
+</div>	
+</head>
+<body>
+	<h1>Thank you for your application!</h1>
+	<p>Your application has been received and will be reviewed by our hiring team. If we are interested in your candidacy, we will contact you to schedule an interview.</p>
+</body>
+</html>
